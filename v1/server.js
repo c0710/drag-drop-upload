3c17cd0483a136858661eaa8730635d86844907d
王成成 <wcc49552@ly.com>
2018-04-16 17:01:53 +0800

    接口对接

--- v1/server.js
+++ v1/server.js
@@ -11,7 +11,16 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
-var server = app.listen(9000, function () {
+/**
+ *  提交接口
+ *  upload.array('file')表示上传一个名为file文件数组
+ * */
+app.post('/uploadFile', upload.array('files'), function (req, res, next) {
+    console.log('upload!')
+    res.end('upload!')
+})
+
+const server = app.listen(9000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log('server start!', host, port)
